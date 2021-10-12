6d8c7797cf7b7f892a7ac1a74669bb4788c455c8
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-12 15:48:59 -0400

    1st commit pseudocode

---
+++ exercise41/src/main/java/baseline/solution41.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 41
+ *  Copyright 2021 Michael Andrich
+ */
+
+package baseline;
+
+class nameSort{
+    // create a class to sort through txt file names
+    // put input.txt names into an array list
+    // make sure the list is alphabetical and "Last, First"
+    // also count how many names are inputted and print out the amount
+}
+
+class printOutput {
+    // create an output file named exercise41_output.txt
+    // format the output file
+}
+
+
+
+public class solution41 {
+    // read in input.txt
+    // declare the array list
+    // call nameSort into input.txt
+    // write all data to output.txt once sorted
+}

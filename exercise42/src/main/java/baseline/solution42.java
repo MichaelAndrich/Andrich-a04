01c5e072ffd5d81ee9346798793ba504af01f6be
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-15 01:51:41 -0400

    1st commit pseudocode

---
+++ exercise42/src/main/java/baseline/solution42.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 42
+ *  Copyright 2021 Michael Andrich
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class solution42 {
+    // create scanner to read input
+    public void readFile(...){
+        Scanner sc = new Scanner();
+
+        // create a loop to read input.txt file with CSV format
+        while(sc.hasNext()){
+
+            // split the string input
+            String[] temp = n.split(",");
+            // print out with format
+            System.out.printf(...);
+        }
+
+
+
+        // print out final table
+        public void main(String[] args){
+
+        }
+    }
+}

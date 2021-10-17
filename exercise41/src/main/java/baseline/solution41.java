4b5f2d004ffc78491342c74a5e623c068d798947
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-17 16:39:19 -0400

    3rd commit with docs folder and plantuml (not final diagram)

--- exercise41/src/main/java/baseline/solution41.java
+++ exercise41/src/main/java/baseline/solution41.java
@@ -10,45 +10,59 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-class fileNames{
-    // create a class to sort through txt file names
-    // put input.txt names into an array list
-    public void fileNames(Scanner input, List<String> names){
-        while(input.hasNextLine()){
-            names.add(input.nextLine());
-        }
-    }
-    // make sure the list is alphabetical and "Last, First"
-    // also count how many names are inputted and print out the amount
-}
-
-class printOutput {
-    // create an output file named exercise41_output.txt
-    public void printOutput(List<String> names) throws IOException {
-        FileWriter output = new FileWriter("exercise41_output.txt");
-        // format the output file
-        output.write("-----------\n");
-
-        for(String i: names){
-            output.write(i+ "\n");
-        }
-        output.close();
-    }
-}
-
-
 
 public class solution41 {
+
+    class fileNames{
+        public fileNames(Scanner inputFile, List<String> names) {
+        }
+
+        // create a class to sort through txt file names
+        // put input.txt names into an array list
+        public void fileNames(Scanner input, List<String> names){
+            while(input.hasNextLine()){
+                names.add(input.nextLine());
+            }
+        }
+        // make sure the list is alphabetical and "Last, First"
+        // also count how many names are inputted and print out the amount
+}
+
+    class printOutput {
+        // create an output file named exercise41_output.txt
+        public void printOutput(List<String> names) throws IOException {
+            FileWriter output = new FileWriter("exercise41_output.txt");
+            // format the output file
+            output.write("-----------\n");
+
+            for(String i: names){
+                output.write(i+ "\n");
+            }
+            output.close();
+        }
+    }
+
     public void main(String[] args) throws FileNotFoundException{
         // read in input.txt
         Scanner inputFile = new Scanner(new File("exercise41_input.txt"));
         // declare the array list
         List<String> names = new List<String>();
-    }
+        new fileNames(inputFile, names);
+        // sort the names
+        Collections.sort(names, String.CASE_INSENSITIVE_ORDER);
+        // close after reading the file
+        inputFile.close();
 
-    // call nameSort into input.txt
-    // write all data to output.txt once sorted
+        try {
+            printOutput(names);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
 }

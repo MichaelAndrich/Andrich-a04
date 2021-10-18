c57ac9c7cc68a63a1dc2e9e3a5289b02fed854bb
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-17 21:20:53 -0400

    2nd commit final code (maybe)

--- exercise43/src/main/java/baseline/solution43.java
+++ exercise43/src/main/java/baseline/solution43.java
@@ -7,25 +7,58 @@
 package baseline;
 
 
+import java.net.URI;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class solution43 {
-    public void main(String[] args){
+    public static void main(String[] args){
         // scan in the input of user
-        Scanner newScan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        String siteName;
+        String author;
+        String inJS;
+        String inCSS;
 
         // read input of the site name
+        System.out.printf("Site name: ");
+        siteName = sc.nextLine();
+
         // "" author
+        System.out.printf("Author: ");
+        author = sc.nextLine();
+
         // read input of y or n for JS folder
+        System.out.printf("Do you want a folder for JavaScript? ");
+        inJS = sc.nextLine();
+
         // "" for CSS folder
+        System.out.printf("Do you want a folder for CSS? ");
+        inCSS = sc.nextLine();
         // store all inputs as strings
 
         // call websiteGenerator
-        // set the author, website name and y or n on the different type of folders
+
+        websiteGenerator newWebsite = new websiteGenerator();
+
+        // set the author, website name
+        newWebsite.siteName = siteName;
+        newWebsite.author = author;
+
+        // create path for output
+        newWebsite.path = Paths.get(URI.create("file:///ThisPC/Desktop/JavaObject/Andrich-a04/exercise43/src/main/docs"));
 
         // create the website along with the HTML file
+        System.out.printf("Created " + newWebsite.createWebsite());
+        System.out.printf("Created " + newWebsite.createHTML());
 
         // output the created JS or CSS files if inputted y
+        if(inJS.equals("y"))
+            System.out.printf("Created " + newWebsite.createJSFolder());
+
+        if(inCSS.equals("y"))
+            System.out.printf("Created " + newWebsite.createCSSFolder());
 
     }
 }
--- exercise43/src/main/java/baseline/websiteGenerator.java
+++ exercise43/src/main/java/baseline/websiteGenerator.java
@@ -3,35 +3,73 @@
 package baseline;
 
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Path;
 
 public class websiteGenerator {
     // creates the characteristics of the website
     // author, name of website, and path
+    String siteName;
+    String author;
+    Path path;
 
     // make a function that creates a website folder
     public String createWebsite(){
 
         // set the directory of the created folder
+        String directory = path + siteName;
+        File newFolder = new File(directory);
+
         // return the directory
+        newFolder.mkdir();
+        return directory;
     }
 
     // make a function that creates a JS folder
     public String createJSFolder() {
         // same as before, set the directory of the created folder
+        String directory = path + siteName + "/js";
+        File newFolder = new File(directory);
+
         // return the directory
+        newFolder.mkdir();
+        return directory;
     }
 
     // make a function that creates a CSS folder
     public String createCSSFolder() {
         // same as before, set the directory path of the created folder
+        String directory = path + siteName + "/css";
+        File newFolder = new File(directory);
+
         // return the directory
+        newFolder.mkdir();
+        return directory;
     }
 
     // make a function that creates an HTML file
     public String createHTML() {
         // set the path
+        String directory = path + siteName + "/index.html";
+
+        String htmlChar = "<title> " + siteName + "</title>\n<meta> " + author + " </meta>";
+
         // create the file
-        // write to the file
+        File file = new File(directory);
+        try {
+            // write to the file
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(htmlChar);
+            writer.close();
+            return directory;
+
+        } catch (Exception ex) {
+            return "Failure";
+        }
+
     }
 
 }
+

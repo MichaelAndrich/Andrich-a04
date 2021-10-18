7bdb2223ac855a1dfd1b3028ed64684f1ed2b00f
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-17 22:32:16 -0400

    1st commit pseudocode

---
+++ exercise44/src/main/java/baseline/solution44.java
@@ -0,0 +1,54 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution 44
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+
+package baseline;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class solution44 {
+    public static void main(String[] args) {
+        // read in the JSON file
+
+        // read internal values with a JSON object
+
+        // read the products of the file
+
+        // use bufferedReader to take in user input
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        // string variable of the users input
+        String product;
+
+        // if no input is entered, then no value is found
+        int x = 0;
+
+        // prompt user for input on product
+        System.out.printf("What is the product name? ");
+        product = bufferedReader.readLine();
+
+        // create iterator that will loop
+
+        while (){
+            // create a new JSON
+
+            // read in the name string
+
+            // now compare to the given input
+            if(product.toLowerCase().equals(product.toLowerCase())){
+                // print out the name, price and quantity
+            }
+        }
+        if (flag == 0){
+            // value not found
+        }
+        catch(Exception e){
+
+        }
+    }
+
+}
